4566488672a450c0e65945d56f3455104d91a54e
mironmahmud <mironbonny1994@gmail.com>
2021-11-21 22:26:19 +0600

    foodomaa-v1.0

---
+++ vendor/slickslider/slick-call.js
@@ -0,0 +1,48 @@
+
+
+// FOR BANNER SECTION SLIDER
+$('.banner-slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    speed: 800,
+    prevArrow: '<i class="material-icons dandik"></i>',
+    nextArrow: '<i class="material-icons bamdik"></i>',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                variableWidth: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                variableWidth: true,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        }
+    ]
+});
+
